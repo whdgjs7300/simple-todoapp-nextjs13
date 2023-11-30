9558d4afae3d468a2212629a697e6e72c344070c
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-11-30 22:22:23 +0900

    11/30 uuid, json server Post 요청 및 응답 (Todo 추가 기능 구현)

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,10 +1,27 @@
 "use client"
-import React, { useState } from 'react'
+import React, { FormEventHandler, useState } from 'react'
 import { FaPlus } from "react-icons/fa6";
 import Modal from './Modal';
+import { addTodo } from '@/api';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = () => {
+    const router = useRouter();
     const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newTaskValue, setNewTaskValue] = useState<string>('');
+
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        await addTodo({
+            id : uuidv4(),
+            text : newTaskValue,
+        })
+        // 서밋 후 인풋 벨류 초기화
+        setNewTaskValue("");
+        setModalOpen(false);
+        router.refresh();
+    }
 
     return (
     <div>
@@ -15,7 +32,20 @@ const AddTask = () => {
 
         <Modal 
         setModalOpen = {setModalOpen} 
-        modalOpen= {modalOpen}/>
+        modalOpen= {modalOpen}> 
+            <form onSubmit={handleSubmitNewTodo}>
+                <h3 className='font-bold text-lg'>Add new Task</h3>
+                <div className='modal-action'>
+                <input 
+                value={newTaskValue} 
+                type="text" 
+                placeholder="Type here" 
+                className="input input-bordered w-full w-full"
+                onChange={e => setNewTaskValue(e.target.value)} />
+                <button type='submit' className='btn'>submit</button>
+                </div>
+            </form>
+        </Modal >
     </div>
 )
 }
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 interface ModalProps {
     modalOpen : boolean
     setModalOpen : (open : boolean)=> boolean | void;
+    children : React.ReactNode
 }
 
-const Modal : React.FC<ModalProps> = ({modalOpen, setModalOpen}) => {
+const Modal : React.FC<ModalProps> = ({modalOpen, setModalOpen , children}) => {
     return (
     <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
         <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click the button below to close</p>
+            
+            {children}
             <div className="modal-action">
             <form method="dialog">
                 {/* if there is a button in form, it will close the modal */}
                 <button onClick={()=> setModalOpen(false)}
                 className="btn">Close</button>
             </form>
+            
             </div>
+            
         </div>
     </div>
 )
